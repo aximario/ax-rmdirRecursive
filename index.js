0962de06a24f86c82bfcf2aa37d63ad7031d68b6
阿希 <aximario@qq.com>
2016-09-08 11:48:12 +0800

    chore: 完成同步删除

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const rmdirRecursiveSync = function (file) {
+	let queue = [],
+		stack = [];
+	queue.push(file);
+
+	while (queue.length) {
+		let cn = queue.shift();
+		let files = fs.readdirSync(cn);
+		let stat, adrs;
+		files.forEach(i => {
+			adrs = path.resolve(cn, i);
+			stat = fs.lstatSync(adrs);
+			if (stat.isFile() || stat.isSymbolicLink()) {
+				fs.unlinkSync(adrs);
+			} else {
+				stack.push(adrs);
+				queue.push(adrs);
+			}
+		});
+	}
+	for (let i = stack.length - 1; i > -1; i--) {
+		fs.rmdirSync(stack[i])
+	}
+	fs.rmdirSync(file);
+};
+
+
+module.exports = {
+	rmdirRecursiveSync: rmdirRecursiveSync
+};
